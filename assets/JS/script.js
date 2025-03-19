2d29cfd5db8608c57779693e45e5bfb1d7edf7fb
Mark <markhallett5@gmail.com>
2025-03-19 22:15:52 +0000

    added 10 questions in script.js

---
+++ assets/JS/script.js
@@ -0,0 +1,93 @@
+const questions = [
+    {
+        question: "What is a checkout score for 156?",
+        answers: [
+            {text: "T19, T19, D20", correct: false},
+            {text: "T20, T20, D20", correct: false},
+            {text: "T20, T14, D16", correct: false},
+            {text: "T20, T20, D19", correct: true},
+        ]
+    }
+    {
+        question: "How many world titles does Michael van Gerwen have?",
+        answers: [
+            { text: "5", correct: false },
+            { text: "1", correct: false },
+            { text: "0", correct: false },
+            { text: "3", correct: true },
+        ]
+    },
+    {
+        question: "What is the highest possible checkout in darts?",
+        answers: [
+            { text: "180", correct: false },
+            { text: "170", correct: true },
+            { text: "160", correct: false },
+            { text: "150", correct: false },
+        ]
+    },
+    {
+        question: "Who won the PDC World Championship on their debut appearance in 2018?",
+        answers: [
+            { text: "Nathan Aspinall", correct: false },
+            { text: "Glen Durrant", correct: false },
+            { text: "Rob Cross", correct: true },
+            { text: "Dimitri Van Den Bergh", correct: false },
+        ]
+    },
+    {
+        question: "Which player is nicknamed “Bully Boy",
+        answers: [
+            { text: "Nathan Aspinall", correct: false },
+            { text: "Joe Cullen", correct: false },
+            { text: "Jonny Clayton", correct: false },
+            { text: "Michael Smith", correct: true },
+        ]
+    },
+    {
+        question: "Which player is known for his intense stare at the dartboard and aggressive celebrations?",
+        answers: [
+            { text: "Adrian Lewis", correct: false },
+            { text: "Gerwyn Price", correct: true },
+            { text: "Jonny Clayton", correct: false },
+            { text: "Danny Noppert", correct: false },
+        ]
+    },
+    {
+    question: "What is the standard height from the floor to the bullseye in professional darts?",
+    answers: [
+        { text: "5 feet 4 inches", correct: false },
+        { text: "6 feet", correct: false },
+        { text: "4 feet 10 inches", correct: false },
+        { text: "5 feet 8 inches", correct: true },
+    ]
+},
+{
+question: "In professional darts, what is the “Madhouse”?",
+answers: [
+    { text: "Missing three darts at double 1", correct: false },
+    { text: "Needing double 1 to win", correct: true },
+    { text: "Scoring three consecutive busts", correct: false },
+    { text: "Hitting only single 5s in a turn", correct: false },
+    ]
+},
+{
+    question: "What is the minimum number of darts needed to win a game of 501?",
+    answers: [
+        { text: "12", correct: false },
+        { text: "6", correct: false },
+        { text: "15", correct: false },
+        { text: "9", correct: true },
+        ]
+    },
+    {
+        question: "Phil Taylor won a record 16 World Championships. In which year did he win his first title?",
+        answers: [
+            { text: "1990", correct: true },
+            { text: "1995", correct: false },
+            { text: "1992", correct: false },
+            { text: "1998", correct: false },
+            ]
+        },
+
+]
