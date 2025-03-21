bedbe86785f7a7383fd238ed39189e4ac68e3a43
Mark <markhallett5@gmail.com>
2025-03-21 18:38:05 +0000

    added let keyword for current question and score

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -96,3 +96,6 @@ const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
+let currentQuestionIndex = 0;
+let score = 0;
+
