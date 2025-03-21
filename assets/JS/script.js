4a34027606893f1f5f2c3bac1acbc58ab4aba568
Mark <markhallett5@gmail.com>
2025-03-21 18:32:06 +0000

    added const variables to script.js file

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -91,3 +91,8 @@ answers: [
         },
 
 ]
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
